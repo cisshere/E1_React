32048e2b2934152e7c0b48ad859bc5e7bf89c37b
cisshere <cis.shere@gmail.com>
2023-09-04 12:23:49 -0400

    realizando categorias

--- src/pages/products/Productos.jsx
+++ src/pages/products/Productos.jsx
@@ -10,17 +10,29 @@ import productList from "../../datos/DatoProductos";
 import CardProducto from "./CardContainer";
 
 const ProductosSecction = () => {
+  const [categories, SetCategories] = useState("Todos");
+
+  const categoriaFiltrada = productList.filter((producto) => {
+    if (categories == "Todos") {
+      return true;
+    } else {
+      return producto.categoria === categories;
+    }
+  });
+
   return (
     <ProductsSection>
       <TituloProductos>PRODUCTOS</TituloProductos>
       <BtnLista>
-        <BtnCategoria>Todos</BtnCategoria>
-        <BtnCategoria>Aros</BtnCategoria>
-        <BtnCategoria>Collares</BtnCategoria>
-        <BtnCategoria>Anillos</BtnCategoria>
+        <BtnCategoria onClick={SetCategories("Todos")}>Todos</BtnCategoria>
+        <BtnCategoria onClick={SetCategories("Aros")}>Aros</BtnCategoria>
+        <BtnCategoria onClick={SetCategories("Collares")}>
+          Collares
+        </BtnCategoria>
+        <BtnCategoria onClick={SetCategories("Anillos")}>Anillos</BtnCategoria>
       </BtnLista>
       <ProductosContainer>
-        {productList.map((producto) => (
+        {categoriaFiltrada.map((producto) => (
           <CardProducto {...producto} key={producto.id} />
         ))}
       </ProductosContainer>
