ca57ba59d6cad071544816a19b10bca506d0c555
cisshere <cis.shere@gmail.com>
2023-09-20 15:57:44 -0400

    modificando CarritoDetalle

--- src/pages/carrito/CarritoDetalleStyle.js
+++ src/pages/carrito/CarritoDetalleStyle.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const CarritoSeccion = styled.section`
-padding-top: 6rem;
-`
+  padding-top: 6rem;
+  margin: 0 30%;
+
+  @media(max-width: 900px){
+    margin: 0 15%;
+  }
+
+  @media(max-width: 500px){
+    margin: 0;
+  } 
+`;
